d584e493618ca005ba16aa680fa1f2e2800571c3
ww36user <2650355534_glb@qq.come>
2020-06-06 15:18:26 +0800

    添加了name属性

--- src/main/java/com/qf/entity/Demo.java
+++ src/main/java/com/qf/entity/Demo.java
@@ -4,11 +4,16 @@ package com.qf.entity;
  * @author Wei
  * @createTime 2020-06-06 14:49
  */
+
+/**
+ * 1.提交之前要更新代码
+ * 2.常更新，早提交
+ */
 public class Demo {
     private Integer id;
 
     private Integer age;
-
+    private String name;
     private String email;
     private String ename;
 }
